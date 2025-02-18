04acc51d69f937e8323a2318d4bd2f28911640f5
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2025-02-18 18:34:09 +0600

    execution time measure added

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
+start = time.time()
 response = requests.get("https://jobs.bdjobs.com/jobsearch.asp?log=stats")
 soup = BeautifulSoup(response.content, "html.parser")
 
@@ -13,4 +15,6 @@ for job_listing in job_listings:
     title = job_listing.find(name="a")
     job_titles.append(title.getText().strip())
 
+end = time.time()
+print("Execution time: " , (end - start) * 10**3 , "ms")
 print(job_titles)
